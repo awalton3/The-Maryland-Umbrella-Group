788127dc9b6579f68e56093721fb4b65d0852b07
awalton3 <awalton3@nd.edu>
2018-08-12 20:52:53 -0400

    added left side-nav using angularjs material styling. added functionality in controller

--- src/app/components/tutor/tutor.controller.js
+++ src/app/components/tutor/tutor.controller.js
@@ -1,12 +1,23 @@
-function TutorController() {
+function TutorController($mdSidenav) {
 
-    var ctrl = this;
-    ctrl.$onInit = function () {
+  var ctrl = this;
+  ctrl.$onInit = function() {
+    //define variables
+    ctrl.toggleLeft = buildToggler('left')
 
-    }
+    //define functions
+    ctrl.buildToggler = buildToggler;
+
+  }
+
+  function buildToggler(componentId) {
+    return function() {
+      $mdSidenav(componentId).toggle();
+    };
+  }
 
 }
 
 angular
-    .module('components.tutor')
-    .controller('TutorController', TutorController);
+  .module('components.tutor')
+  .controller('TutorController', TutorController);
