3ab3b380381df240e3569d9c7238b938569fecff
awalton3 <awalton3@nd.edu>
2018-08-16 00:48:33 -0400

    added an upload component to hold all user uploads

---
+++ src/app/components/uploads/upload/upload.component.js
@@ -0,0 +1,11 @@
+var upload = {
+  templateUrl: './upload.html',
+  controller: 'UploadController',
+  bindings: {
+    
+  }
+}
+
+angular
+  .module('components.uploads')
+  .component('upload', upload);
---
+++ src/app/components/uploads/upload/upload.controller.js
@@ -0,0 +1,10 @@
+function UploadController() {
+
+  var ctrl = this;
+  ctrl.$onInit = function() {}
+
+}
+
+angular
+  .module('components.uploads')
+  .controller('UploadController', UploadController);
